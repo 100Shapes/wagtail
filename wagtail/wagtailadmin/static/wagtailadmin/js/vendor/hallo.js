2fb1b027032736c162ac6486c9df30ec29af9f79
Paolo <Paolo@100shapes.com>
2014-08-21 15:41:20 +0100

    removed htmlcleaner

--- wagtail/wagtailadmin/static/wagtailadmin/js/vendor/hallo.js
+++ wagtail/wagtailadmin/static/wagtailadmin/js/vendor/hallo.js
@@ -719,7 +719,9 @@
           editor.html('');
           return setTimeout(function() {
             var cleanPasted, error, pasted, range;
-            cleanPasted = jQuery.htmlClean(pasted, _this.options);
+            
+            cleanPasted = pasted;
+            //cleanPasted = jQuery.htmlClean(pasted, _this.options);
             editor.html(lastContent);
             rangy.restoreSelection(lastRange);
             if (cleanPasted !== '') {
